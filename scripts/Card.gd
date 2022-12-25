e7372add1b92152e8bb13cbb55ba731b1666189a
MichaelStrike <michael.strelecz@gmail.com>
2022-12-25 10:43:11 +0300

    подготовка

--- scripts/Card.gd
+++ scripts/Card.gd
@@ -2,11 +2,6 @@ extends Button
 
 var action = 'teleport'
 
-
-func _on_Card_toggled(button_pressed):
-	if button_pressed:
-		get_parent().raffle('player')
-
 # TRIGGERED
 func battle():
 	pass
@@ -16,11 +11,8 @@ func battle_finished():
 	pass
 func raffle():
 	pass
-func raffle_started():
-	pass
+func raffle_started(pressed):
+	if pressed:
+		print('raffle_started')
 func raffle_finished():
 	pass
-
-
-func teleport(object):
-	object.position = object.position + Vector2.ONE*100
